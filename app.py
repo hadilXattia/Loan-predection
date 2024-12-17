bbb06c5843065cc5d876d1966457d6cb1ed267f2
hadilXattia <110170490+hadilXattia@users.noreply.github.com>
2024-12-17 17:21:26 +0100

    app changed

--- app.py
+++ app.py
@@ -1,49 +1,23 @@
 import streamlit as st
-import utils
-from utils import preprocessdata
-
-# Replace Flask routes with Streamlit UI elements
+import os
 
 def home():
-    st.title("Loan Prediction")
-    st.write("Welcome to the Loan Prediction App!")
+    # Load the 'index.html' from the 'templates' folder
+    with open(os.path.join("templates", "index.html"), "r") as file:
+        home_html = file.read()
+    st.markdown(home_html, unsafe_allow_html=True)
 
 def about():
-    st.title("About")
-    st.write("This is an app to predict loan eligibility based on various parameters.")
+    # Load the 'About.html' from the 'templates' folder
+    with open(os.path.join("templates", "About.html"), "r") as file:
+        about_html = file.read()
+    st.markdown(about_html, unsafe_allow_html=True)
 
 def prediction_form():
-    st.title("Prediction Form")
-    
-    # Input fields
-    Gender = st.selectbox("Gender", ["Male", "Female"])
-    Married = st.selectbox("Married", ["Yes", "No"])
-    Education = st.selectbox("Education", ["Graduate", "Not Graduate"])
-    Self_Employed = st.selectbox("Self Employed", ["Yes", "No"])
-    ApplicantIncome = st.number_input("Applicant Income", min_value=0)
-    CoapplicantIncome = st.number_input("Coapplicant Income", min_value=0)
-    LoanAmount = st.number_input("Loan Amount", min_value=0)
-    Loan_Amount_Term = st.selectbox("Loan Amount Term", [12, 24, 36, 48, 60])
-    Credit_History = st.selectbox("Credit History", ["Yes", "No"])
-    Property_Area = st.selectbox("Property Area", ["Urban", "Semiurban", "Rural"])
+    # Load the 'PredictionForm.html' from the 'templates' folder
+    with open(os.path.join("templates", "PredictionForm.html"), "r") as file:
+        prediction_form_html = file.read()
+    st.markdown(prediction_form_html, unsafe_allow_html=True)
 
-    # Prediction button
-    if st.button("Predict"):
-        prediction = utils.preprocessdata(Gender, Married, Education, Self_Employed, ApplicantIncome,
-                                          CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History,
-                                          Property_Area)
-        st.write(f"Prediction: {prediction}")
-
-# Mapping Streamlit pages
-PAGES = {
-    "Home": home,
-    "About": about,
-    "Prediction Form": prediction_form
-}
-
-# Sidebar for navigation
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-
-# Call the selected page function
-PAGES[selection]()
+# Directly call the home function to display the home page
+home()
